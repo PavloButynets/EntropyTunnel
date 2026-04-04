8b902d76a77133bb971bd968edce4210e47416b5
ktr <pavlobutynez@gmail.com>
2026-04-05 00:41:03 +0300

    Update TunnelSettings and Nginx configuration for HTTPS support

--- EntropyTunnel.Client/Configuration/TunnelSettings.cs
+++ EntropyTunnel.Client/Configuration/TunnelSettings.cs
@@ -2,7 +2,7 @@ namespace EntropyTunnel.Client.Configuration;
 
 public sealed class TunnelSettings
 {
-    public string ServerUrl { get; set; } = "ws://entropy-tunnel.xyz/tunnel";
+    public string ServerUrl { get; set; } = "wss://entropy-tunnel.xyz/tunnel";
     public string PublicDomain { get; set; } = "entropy-tunnel.xyz";
     public string ClientId { get; set; } = "default";
     public int LocalPort { get; set; } = 5173;
